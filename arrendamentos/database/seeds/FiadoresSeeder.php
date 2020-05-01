74d2f8b0df866b57ec0d2033c8c61b082ca33aa2
Daniela Serra <aluno20447@ipt.pt>
2020-05-01 21:20:58 +0100

    Dados dos fiadores

--- arrendamentos/database/seeds/FiadoresSeeder.php
+++ arrendamentos/database/seeds/FiadoresSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Fiadores;
 
 class FiadoresSeeder extends Seeder
 {
@@ -11,6 +12,23 @@ class FiadoresSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Fiadores::create([
+            'nome' => 'Elsa',
+            'data_nascimento' => '1997-06-24',
+            'idade' => '24',
+            'NIF' => '44632834678',
+            'CC' => '462347238',
+            'email' => 'elsa@gmail.com',
+            'telefone' => '34738923478',
+            'morada' => 'Rua da Elsa',
+            'IBAN' => '4738948792',
+            'tipo_particular_empresa' => '3',
+            'cae' => '2',
+            'capital_social' => '2',
+            'setor_actividade' => 'Agricultura',
+            'certidao_permanente' => 'certidao',
+            'num_funcionarios' => '2'
+
+        ]);
     }
 }
