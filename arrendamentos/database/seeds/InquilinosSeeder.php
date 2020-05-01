a5383b4b02d16cdd0d7e150efbe57f9e922b7c30
Daniela Serra <aluno20447@ipt.pt>
2020-05-01 20:52:14 +0100

    adição de inquilinos

--- arrendamentos/database/seeds/InquilinosSeeder.php
+++ arrendamentos/database/seeds/InquilinosSeeder.php
@@ -15,24 +15,25 @@ class InquilinosSeeder extends Seeder
      */
     public function run()
     {
+
         Inquilinos::create([
-            'nome'      => 'Eusebio ',
-            'data_nascimento' => '2018-08-25',
-            'idade' => '23',
-            'NIF' => '32743892464783920',
-            'CC' => '437289476473829',
-            'email'     => 'eusebio@gmail.com',
-            'telefone' => '6437283476',
-            'morada' => 'Rua do Eusebio',
-            'IBAN' => '47382937648392',
-            'tipo_particular_empresa' => '3',
-            'profissao' => 'Agricultor',
-            'vencimento' => '2',
-            'tipo_contrato' => 'semestral',
+            'nome'      => 'Tomas ',
+            'data_nascimento' => '1996-07-28',
+            'idade' => '24',
+            'NIF' => '',
+            'CC' => '53627893',
+            'email'     => 'tomas@gmail.com',
+            'telefone' => '362834234',
+            'morada' => 'Rua do Tomas',
+            'IBAN' => '46328347',
+            'tipo_particular_empresa' => '2',
+            'profissao' => 'Engenheiro',
+            'vencimento' => '3',
+            'tipo_contrato' => 'anual',
             'notas' => 'nada a acrescentar',
-            'cae' => '3',
-            'capital_social' => '2',
-            'setor_actividade' => 'Agricultura',
+            'cae' => '1',
+            'capital_social' => '4',
+            'setor_actividade' => 'Engenharia',
             'certidao_permanente' => 'certidao',
             'num_funcionarios' => '4'
 
