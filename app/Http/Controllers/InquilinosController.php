1dee6f077ede0ec2728963fc3a324b9353d18d8d
Daniela Serra <aluno20447@ipt.pt>
2020-05-10 03:04:55 +0100

    Controller tenant with create, store, edit, update and destroy

--- app/Http/Controllers/InquilinosController.php
+++ app/Http/Controllers/InquilinosController.php
@@ -2,27 +2,159 @@
 
 namespace App\Http\Controllers;
 
-
-use Illuminate\Support\Facades\DB;
 use App\Inquilinos;
+use Illuminate\Http\Request;
 use App\Http\Controllers\Traits\Authorizable;
 use App\Permission;
 use App\Role;
-use App\User;
 use Illuminate\Auth\Events\Registered;
-use App\DataTables\UserDataTable;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rule;
-
-use Illuminate\Http\Request;
+use App\Http\Controllers\InquilinosDataTable;
+use App\Http\Controllers\Session;
 
 class InquilinosController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $inquilinos = Inquilinos::latest()->paginate(5);
+        return view('inquilinos.index',compact('inquilinos'))
+            ->with('i', (request()->input('page', 1) - 1) * 5);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('inquilinos.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+
+            'nome' => 'required',
+            'data_nascimento' => 'required',
+            'idade' => 'required',
+            'NIF' => 'required',
+            'CC' => 'required',
+            'email' => 'required',
+            'telefone' => 'required',
+            'morada' => 'required',
+            'IBAN' => 'required',
+            'tipo_particular_empresa' => 'required',
+            'profissao' => 'required',
+            'vencimento' => 'required',
+            'tipo_contrato' => 'required',
+            'notas' => 'required',
+            'cae' => 'required',
+            'capital_social' => 'required',
+            'setor_actividade' => 'required',
+            'certidao_permanente' => 'required',
+            'num_funcionarios' => 'required',
+
+        ]);
+
+        Inquilinos::create($request->all());
+        return redirect()->route('inquilinos.index')
+             ->with('success','Inquilino created successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Inquilinos  $inquilinos
+     * @return \Illuminate\Http\Response
+     */
     public function show($id)
     {
-        $inquilinos = DB::table('inquilinos')->where('id', $id)->first();
+        $inquilinos = Inquilinos::findOrfail($id);
+        return view('inquilinos.show', compact('inquilinos'));
+    }
 
-        dd($inquilinos);
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Inquilinos  $inquilinos
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $inquilinos = Inquilinos::findOrfail($id);
+        return view('inquilinos.edit', compact('inquilinos'));
+    }
 
-        return view('inquilinos.index', ['inquilinos' => $inquilinos]);
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Inquilinos  $inquilinos
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $inquilinos = Inquilinos::findOrfail($id);
+        $this->validate($request,[
+
+            'nome' => 'required',
+            'data_nascimento' => 'required',
+            'idade' => 'required',
+            'NIF' => 'required',
+            'CC' => 'required',
+            'email' => 'required',
+            'telefone' => 'required',
+            'morada' => 'required',
+            'IBAN' => 'required',
+            'tipo_particular_empresa' => 'required',
+            'profissao' => 'required',
+            'vencimento' => 'required',
+            'tipo_contrato' => 'required',
+            'notas' => 'required',
+            'cae' => 'required',
+            'capital_social' => 'required',
+            'setor_actividade' => 'required',
+            'certidao_permanente' => 'required',
+            'num_funcionarios' => 'required',
+
+        ]);
+
+        $input = $request->all();
+        $inquilinos->fill($input)->save();
+
+
+
+        $request->session()->flash('inquilinos', $input);
+
+        return redirect()->route('inquilinos.index')
+                ->with('success','Inquilino updated successfully.');
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Inquilinos  $inquilinos
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $inquilinos = Inquilinos::findOrfail($id);
+        $inquilinos->delete();
+        return redirect()->route('inquilinos.index')
+                        ->with('success','Inquilino deleted successfully');
     }
 }
