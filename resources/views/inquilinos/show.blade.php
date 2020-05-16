0dbe1135d49750ed803dee3122d91e62dc0af13d
Daniela Serra <aluno20447@ipt.pt>
2020-05-16 01:09:59 +0100

    view updated show tenant

--- resources/views/inquilinos/show.blade.php
+++ resources/views/inquilinos/show.blade.php
@@ -1,208 +1,111 @@
+<?php
+/**
+ *
+ * @var $user \App\User
+ */
+view()->share('pageTitle', $inquilino->name);
+view()->share('hideSubHeader', true);
+?>
 @extends('layouts.app')
-
 @section('breadcrumbs')
     {{ Breadcrumbs::render('inquilinos.show', $inquilino) }}
 @endsection
 @section('content')
-
-    <div class="row">
-
-        <div class="jumbotron text-center">
-
-            <div class="pull-left">
-
-                <h2> Show {{ $inquilino->nome }}</h2>
-
+    <div class="kt-portlet">
+        <div class="kt-portlet__head">
+            <div class="kt-portlet__head-label">
+                <h3 class="kt-portlet__head-title">
+                    {{ $inquilino->name }}
+                </h3>
             </div>
-
-            <div class="pull-right">
-
-                <a class="btn btn-primary" href="{{ route('inquilinos.index') }}"> Back</a>
-
-            </div>
-
         </div>
-
+        <div class="kt-portlet__body">
+            <!--begin::Section-->
+            <div class="kt-section">
+                <div class="kt-section__content">
+                    <table class="table table-striped">
+                        <tbody>
+                            <tr>
+                                <th scope="row">{{ __('Name') }}</th>
+                                <td>{{ $inquilino->nome }}</td>
+                            </tr>
+                             <tr>
+                                <th scope="row">{{ __('Date of birth') }}</th>
+                                <td>{{ $inquilino->data_nascimento }}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">{{ __('Age') }}</th>
+                                <td>{{ $inquilino->idade }}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">{{ __('NIF') }}</th>
+                                <td>{{ $inquilino->NIF }}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">{{ __('CC') }}</th>
+                                <td>{{ $inquilino->CC }}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">{{ __('Email') }}</th>
+                                <td><a href="mailto:{{ $inquilino->email }}">{{ $inquilino->email }}</a></td>
+                            </tr>
+                            <tr>
+                                <th scope="row">{{ __('Telephone') }}</th>
+                                <td>{{ $inquilino->telefone }}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">{{ __('Address') }}</th>
+                                <td>{{ $inquilino->morada }}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">{{ __('IBAN') }}</th>
+                                <td>{{ $inquilino->IBAN }}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">{{ __('Particular type of company') }}</th>
+                                <td>{{ $inquilino->tipo_particular_empresa }}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">{{ __('Career') }}</th>
+                                <td>{{ $inquilino->profissao }}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">{{ __('Pay') }}</th>
+                                <td>{{ $inquilino->vencimento }}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">{{ __('Type of contract') }}</th>
+                                <td>{{ $inquilino->tipo_contrato }}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">{{ __('Grades') }}</th>
+                                <td>{{ $inquilino->notas }}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">{{ __('CAE') }}</th>
+                                <td>{{ $inquilino->cae }}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">{{ __('Share capital') }}</th>
+                                <td>{{ $inquilino->capital_social }}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">{{ __('Activity sector') }}</th>
+                                <td>{{ $inquilino->setor_actividade }}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">{{ __('Permanent certificate') }}</th>
+                                <td>{{ $inquilino->certidao_permanente }}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">{{ __('Number of employees') }}</th>
+                                <td>{{ $inquilino->num_funcionarios }}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <!--end::Section-->
+        </div>
     </div>
-
-    <div class="row">
-
-        <div class="col-xs-12 col-sm-12 col-md-12">
-
-            <div class="form-group">
-
-                <h3 class="col-md-4 text-right">Nome:{{ $inquilino->nome }}</h3>
-            </div>
-
-        </div>
-
-        <br />
-
-        <div class="col-xs-12 col-sm-12 col-md-12">
-
-            <div class="form-group">
-
-                <h3 class="col-md-4 text-right">Data de Nascimento:{{ $inquilino->data_nascimento }}</h3>
-
-            </div>
-
-        </div>
-
-        <div class="col-xs-12 col-sm-12 col-md-12">
-
-            <div class="form-group">
-
-                <h3 class="col-md-4 text-right">Idade:{{ $inquilino->idade }}</h3>
-
-            </div>
-
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-
-            <div class="form-group">
-
-                <h3 class="col-md-4 text-right">NIF:{{ $inquilino->NIF }}</h3>
-
-            </div>
-
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-
-            <div class="form-group">
-
-                <h3 class="col-md-4 text-right">CC:{{ $inquilino->CC }}</h3>
-
-            </div>
-
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-
-            <div class="form-group">
-
-                <h3 class="col-md-4 text-right">Email:{{ $inquilino->email }}</h3>
-
-            </div>
-
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-
-            <div class="form-group">
-
-                <h3 class="col-md-4 text-right">Telefone:{{ $inquilino->telefone }}</h3>
-
-            </div>
-
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-
-            <div class="form-group">
-
-                <h3 class="col-md-4 text-right">Morada:{{ $inquilino->morada }}</h3>
-
-            </div>
-
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-
-            <div class="form-group">
-
-                <h3 class="col-md-4 text-right">IBAN:{{ $inquilino->IBAN }}</h3>
-
-            </div>
-
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-
-            <div class="form-group">
-
-                <h3 class="col-md-4 text-right">Tipo particular de empresa:{{ $inquilino->tipo_particular_empresa }}</h3>
-
-            </div>
-
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-
-            <div class="form-group">
-
-                <h3 class="col-md-4 text-right">Profissão:{{ $inquilino->profissao }}</h3>
-
-            </div>
-
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-
-            <div class="form-group">
-
-                <h3 class="col-md-4 text-right">Vencimento:{{ $inquilino->vencimento }}</h3>
-
-            </div>
-
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-
-            <div class="form-group">
-
-                <h3 class="col-md-4 text-right">Tipo de Contrato:{{ $inquilino->tipo_contrato }}</h3>
-
-            </div>
-
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-
-            <div class="form-group">
-
-                <h3 class="col-md-4 text-right">Notas:{{ $inquilino->notas }}</h3>
-
-            </div>
-
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-
-            <div class="form-group">
-
-                <h3 class="col-md-4 text-right">CAE:{{ $inquilino->cae }}</h3>
-
-            </div>
-
-        </div>
-
-        <div class="col-xs-12 col-sm-12 col-md-12">
-
-            <div class="form-group">
-
-                <h3 class="col-md-4 text-right">Capital Social:{{ $inquilino->capital_social }}</h3>
-
-            </div>
-
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-
-            <div class="form-group">
-
-                <h3 class="col-md-4 text-right">Setor de Actividade:{{ $inquilino->setor_actividade }}</h3>
-
-            </div>
-
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-
-            <div class="form-group">
-
-                <h3 class="col-md-4 text-right">Certidão Permanente:{{ $inquilino->certidao_permanente }}</h3>
-
-            </div>
-
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-
-            <div class="form-group">
-
-                <h3 class="col-md-4 text-right">Nº Funcionários:{{ $inquilino->num_funcionarios }}</h3>
-
-            </div>
-
-        </div>
-
-    </div>
-
 @endsection
