79ef4e3b6d370f2694de4c757503221adf338d0e
Daniela Serra <aluno20447@ipt.pt>
2020-06-18 21:17:53 +0100

    contract model

--- app/Contrato.php
+++ app/Contrato.php
@@ -3,9 +3,31 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Traits\LoadDefaults;
 
 class Contrato extends Model
 {
+
+    use LoadDefaults;
+
+    protected $fillable = [
+        'id',
+        'valorRenda',
+        'tipoContrato',
+        'inicioContrato',
+        'fimContrato' ,
+        'renovavel' ,
+        'isencaoBeneficio',
+        'retencaoFonte',
+        'dataLimitePagamento' ,
+        'estado',
+        'encargos',
+        'caucao',
+        'metodoPagamento',
+        'rendasAvanco'
+    ];
+
+
     public function imovel()
     {
         return $this->belongsTo('App\Imovel');
