e6305e6e3e37f125e77f33afe6d2ce376a341da8
Daniela Serra <aluno20447@ipt.pt>
2020-06-25 20:53:49 +0100

    model das rendas

--- app/Renda.php
+++ app/Renda.php
@@ -3,9 +3,27 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Traits\LoadDefaults;
 
 class Renda extends Model
 {
+
+    use LoadDefaults;
+
+    protected $fillable = [
+        'id',
+        'valorPagar',
+        'dataPagamento',
+        'metodoPagamento',
+        'valorPago' ,
+        'valorDivida' ,
+        'estado',
+        'dataLimitePagamento',
+        'notas' ,
+        'dataRecibo'
+    ];
+
+
     public function contrato()
     {
         return $this->belongsTo('App\Contrato');
