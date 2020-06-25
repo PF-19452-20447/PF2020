7556a3fab4fdb401050e205514583dfad05a207f
Daniela Serra <aluno20447@ipt.pt>
2020-06-25 20:55:45 +0100

    view edit rendas

---
+++ resources/views/rendas/edit.blade.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ *
+ * @var $renda \App\Renda
+ */
+view()->share('pageTitle', $renda->valorPagar);
+view()->share('hideSubHeader', true);
+?>
+@extends('layouts.app')
+@section('breadcrumbs')
+    {{ Breadcrumbs::render('rendas.edit', $renda) }}
+@endsection
+@section('content')
+@can('adminApp')
+    <div class="kt-portlet">
+        <div class="kt-portlet__head">
+            <div class="kt-portlet__head-label">
+                <h3 class="kt-portlet__head-title">
+                    {{ $renda->valorPagar }}
+                </h3>
+            </div>
+        </div>
+        @include('rendas._form')
+    </div>
+    @endcan
+@endsection
