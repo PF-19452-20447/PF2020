e9aa558ad0db9dbec687b829a7cbd423087d4c8c
Daniela Serra <aluno20447@ipt.pt>
2020-08-13 21:25:04 +0100

    o inquilino já consegue ver as propriedades.

--- app/Inquilino.php
+++ app/Inquilino.php
@@ -142,6 +142,10 @@ class Inquilino extends Model implements HasMedia
         return $this->hasMany('App\Renda'); /*, 'inquilino_id', 'id');*/
     }
 
+    public function imoveis(){
+        return $this->hasMany('App\Imovel');
+    }
+
     public function registerMediaCollections(): void
     {
         $this->addMediaCollection('images')
